835a1e90aa87168c6104f72f9118f5fd9d7f8e87
Thanhlcc <lccongthanh1102@gmail.com>
2024-02-09 08:34:37 +0700

    first commit

---
+++ src/test/java/com/project/university/specializedproject/bktechstoreconfigserver/BktechstoreConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.university.specializedproject.bktechstoreconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BktechstoreConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
